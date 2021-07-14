e635bcd61f07b0e07052730304d0271ad2e25c16
A.C.Kimari <annchristine704@gmail.com>
2021-07-13 17:27:39 -0700

    added javascript codes for menu content to link to page

---
+++ script.js
@@ -0,0 +1,35 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+
+const mobileMenu = () => {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+};
+
+menu.addEventListener('click', mobileMenu);
+
+const highlightMenu = () => {
+    const element = document.querySelector('.highlight');
+    const aboutMenu = document.querySelector('#aboutme-page');
+    const projectMenu = document.querySelector('#projects-page');
+    const contactMenu = document.querySelector('#contactme-page');
+    let scrollPos = window.scrollY
+
+    if (window.innerWidth > 960 && scrollPos < 600) {
+        aboutMenu.classList.add('highlight')
+        projectMenu.classList.add('highlight')
+        return
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+        projectMenu.classList.add('highlight')
+        contactMenu.classList.add('highlight')
+        return
+    }
+
+    if ((element && window.innerWidth < 960 && scrollPos < 600) || element) {
+        element.classList.remove('highlight')
+    }
+};
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
